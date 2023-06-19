8739e755e2e1c90c7b31fb7e511e7966fbb6ff4b
a186r <hi.aewalle@gmail.com>
2023-06-19 11:22:34 +0800

    feat: op

--- scripts/starknet.ts
+++ scripts/starknet.ts
@@ -8,30 +8,37 @@ import Table from 'cli-table3';
 dotenv.config();
 
 const { APIKEY_STARKNET } = process.env;
+
+if (!APIKEY_STARKNET) {
+    console.error('Please set the environment variable APIKEY_STARKNET.');
+    process.exit(1);
+}
+
 const MAX_DISPLAY_LENGTH = 9;
-const queue = new PQueue({ interval: 0, intervalCap: 1 });
+const queue = new PQueue({ interval: 0, intervalCap: 5 });
 
 const url = `https://starknet-mainnet.g.alchemy.com/v2/${APIKEY_STARKNET}`;
+
+interface AddressData {
+    Address: string;
+    Txs: string | null;
+}
 
 async function getNonce(address: string): Promise<string | null> {
     const payload = {
         jsonrpc: '2.0',
         id: 1,
         method: 'starknet_getNonce',
-        params: [
-            "latest",
-            address
-        ]
+        params: ["latest", address]
     };
 
-    const nonce = axios.post(url, payload)
-        .then((response: { data: { result: any; }; }) => {
-            return response.data.result;
-        })
-        .catch((error: any) => {
-            return null;
-        });
-    return nonce;
+    try {
+        const response = await axios.post(url, payload);
+        return response.data.result;
+    } catch (error) {
+        console.error(`Failed to get nonce for address ${address}: ${error}`);
+        return null;
+    }
 }
 
 function shortenAddress(address: string): string {
@@ -44,16 +51,9 @@ function shortenAddress(address: string): string {
     return `${start}...${end}`;
 }
 
-async function processAddress(address: string) {
+async function processAddress(address: string): Promise<AddressData> {
     const nonce = await getNonce(address);
     const shortenedAddress = shortenAddress(address);
-
-    if (!nonce) {
-        return {
-            Address: shortenedAddress,
-            Txs: '0',
-        };
-    }
 
     return {
         Address: shortenedAddress,
@@ -67,28 +67,20 @@ async function main() {
 
     const addressPromises = fileLines.map(address => queue.add(() => processAddress(address)));
 
-    const addressData = await Promise.all(addressPromises);
+    const addressData = (await Promise.all(addressPromises)).filter(data => data.Txs !== null);
 
-    // Define table structure
     const table = new Table({
         head: ['Index', 'Address', 'Txs'],
         colWidths: [12, 15, 5]
     });
 
-    // Populate table data
-    addressData.filter(Boolean).forEach((data, index) => {
-        // Txs
-        const txs = parseInt(data.Txs, 16);
+    addressData.forEach((data, index) => {
+        const txs = parseInt(data.Txs!, 16);
         const coloredTxs = txs > 30 ? chalk.green(txs + '') : txs;
-
-        // Index
         const printedIndex = index <= 24 ? 'Braavos' + (index + 1) : 'Argent' + (index - 24);
-
-        // Add index to the table
         table.push([printedIndex, data.Address, coloredTxs]);
     });
 
-    // Print table
     console.log(table.toString());
 }
 
